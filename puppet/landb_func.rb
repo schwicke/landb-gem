2eb2167edb59c02d64e5e092d2a92e2ff1f652d2
Anastasis Andronidis <anastasis90@yahoo.gr>
2012-08-22 16:20:53 +0200

    typos and minor fix

--- puppet/landb_func.rb
+++ puppet/landb_func.rb
@@ -1,13 +1,14 @@
 module Puppet::Parser::Functions
   newfunction(:landb_func, :type => :rvalue, :doc => "A puppet funtion to integrate with LanDB") do |args|
 
+    require 'rubygems'
     require 'yaml'
     require 'landb'
 
     option_hash = args[0]
 
     config = begin
-      YAML.load(File.open("/var/lib/puppet/lib/puppet/parser/functions"))
+      YAML.load(File.open("/etc/puppet/landb_config.yml"))
     rescue ArgumentError => e
       puts "Could not parse YAML: #{e.message}"
     end
@@ -20,9 +21,9 @@ module Puppet::Parser::Functions
     responce = client_method.call option_hash["method_arguments"]
 
     if option_hash["responce_info"].first.instance_of?(Array)
-      responce = responce.get_values_for_paths option_hash["responce_info"]
+      responce = responce.get_values_for_paths option_hash["response_info"]
     else
-      responce = responce.get_value_for_path option_hash["responce_info"]
+      responce = responce.get_value_for_path option_hash["response_info"]
     end
 
     responce
