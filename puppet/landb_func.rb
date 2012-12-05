c48ef788be75cf7d3cccb681d17481627db4dba0
Daniel Lobato <elobatocs@gmail.com>
2012-12-05 16:20:36 +0100

    Removed binaries from repository, updated gemspec

--- puppet/landb_func.rb
+++ puppet/landb_func.rb
@@ -1,17 +1,17 @@
 module Puppet::Parser::Functions
   newfunction(:landb_func, :type => :rvalue, :doc => "A puppet function to integrate with LanDB") do |args|
 
-    require 'rubygems'
+    require 'ruby-debug'
     require 'yaml'
     require 'landb'
 
     option_hash = args[0]
 
     config = begin
-      YAML.load(File.open("/etc/puppet/landb_config.yml"))
-    rescue ArgumentError => e
-      puts "Could not parse YAML: #{e.message}"
-    end
+               YAML.load(File.open("/etc/puppet/landb_config.yaml"))
+             rescue ArgumentError => e
+               puts "Could not parse YAML: #{e.message}"
+             end
 
     LandbClient.set_config(config)
     client = LandbClient.instance
@@ -20,12 +20,13 @@ module Puppet::Parser::Functions
 
     responce = client_method.call option_hash["method_arguments"]
 
-    if option_hash["responce_info"].first.instance_of?(Array)
-      responce = responce.get_values_for_paths option_hash["response_info"]
+    if option_hash["response_info"].first.instance_of?(Array)
+      responce = responce.get_values_for_paths(option_hash["response_info"]).join('-')
     else
       responce = responce.get_value_for_path option_hash["response_info"]
     end
 
     responce
   end
-end
+end 
+
